9c890cb203154002d2081161e40eb66a0f42b506
Nicholas D. Socci <socci@lux>
2014-01-19 15:10:52 -0500

    PE version of clipAdapters finished (DEBUG ON)

--- bin/clipAdapters.sh
+++ bin/clipAdapters.sh
@@ -7,8 +7,33 @@
 
 
 ADAPTER=$1
-FASTQ=$2
+FASTQ1=$2
+FASTQ2=$3
+BASE1=$SCRATCH/$(basename $FASTQ1)
+BASE2=$SCRATCH/$(basename $FASTQ2)
 
-echo $SCRATCH
+if [ "$MINLENGTH" == "" ]; then
+    MINLENGTH=35
+    echo Default MINLENGTH=$MINLENGTH set
+fi
 
-#cutadapt -a $ADAPTER -e .2
+if [ "$ERROR" == "" ]; then
+    ERROR=0.2
+    echo Default ERROR=$ERROR set
+fi
+
+##
+# Debug limit
+gzcat $FASTQ1 | head -40000 >$SCRATCH/tmp1.fastq
+gzcat $FASTQ2 | head -40000 >$SCRATCH/tmp2.fastq
+FASTQ1=$SCRATCH/tmp1.fastq
+FASTQ2=$SCRATCH/tmp2.fastq
+
+cutadapt -m $MINLENGTH -a $ADAPTER -e $ERROR \
+    --paired-output ${BASE2}.tmp.fastq -o ${BASE1}.tmp.fastq $FASTQ1 $FASTQ2 \
+    > ${BASE1}.log
+
+cutadapt -m $MINLENGTH -a $ADAPTER -e $ERROR \
+    --paired-output ${BASE1}___CLIP.fastq -o ${BASE2}___CLIP.fastq ${BASE2}.tmp.fastq ${BASE1}.tmp.fastq \
+    > ${BASE2}.log
+
