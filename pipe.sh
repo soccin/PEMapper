3a09431e2ac5898107758e4e3660068e3c1fbd0c
Nicholas D. Socci <socci@lux>
2014-01-19 15:11:26 -0500

    PE version of clipAdapters finished (DEBUG ON)

--- pipe.sh
+++ pipe.sh
@@ -45,10 +45,17 @@ fi
 echo SAMPLENAME=$SAMPLENAME
 
 export SCRATCH=$(pwd)/_scratch
+mkdir -p $SCRATCH
 
 ##
 # HiSeq TrueSeq maximal common adapter
 
 ADAPTER="AGATCGGAAGAGC"
-clipAdapters.sh $ADAPTER $FASTQ
+
+for FASTQ in $SAMPLEDIR/*_R1_???.fastq.gz; do
+    BASE=$SCRATCH/$(basename $FASTQ)
+    clipAdapters.sh $ADAPTER $FASTQ ${FASTQ/_R1_/_R2_}
+    CLIPSEQ1=$SCRATCH/$(basename $FASTQ)___CLIP.fastq
+    CLIPSEQ2=$SCRATCH/$(basename ${FASTQ/_R1_/_R2_})___CLIP.fastq
+done
 
