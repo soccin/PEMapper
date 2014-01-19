dea73a4f3bc837b4c8867fedc1f4bcc532698519
Nicholas D. Socci <socci@lux>
2014-01-19 18:09:41 -0500

    Added more stuff to filter in checkforerrors

--- checkForErrors.sh
+++ checkForErrors.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat *.e* | egrep -v "(M::|^.main|^INFO|Runtime|Elapsed time:|net.sf.picard.sam|Java HotSpot)" | sort | uniq
+cat *.e* | egrep -v "(M::|^.main|^INFO|Runtime|Elapsed time:|net.sf.picard|Java HotSpot)" | sort | uniq
