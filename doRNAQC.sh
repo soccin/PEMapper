0c968b60bc77fb25c29b9bc5e2128a6db287aee0
Nicholas D. Socci <socci@lux>
2015-04-17 18:11:43 -0400

    Added RNASeq stats (for strand test)

---
+++ doRNAQC.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+GENOME_FASTA=/ifs/data/bio/Genomes/M.musculus/mm9/mouse_mm9__All.fa
+GENESFILE=/home/socci/Work/SeqAna/Pipelines/db/MM9/refFlat__mm9.txt.gz
+RIBOFILE=/home/socci/Work/SeqAna/Pipelines/db/MM9/ribosomal.interval_file
+
+#GENOME_FASTQ=/ifs/data/bio/assemblies/H.sapiens/hg19/hg19.fasta
+#GENESFILE=/home/socci/Work/SeqAna/Pipelines/db/HG19/refFlat__hg19.txt.gz
+#RIBOFILE=/home/socci/Work/JonesD2/AminE/RNAEditting/VarScan_20150227/ribosomal.interval_file
+
+BAM=$1
+BASE=$(basename $BAM | sed 's/.bam//')
+source ~/Work/SGE/sge.sh
+
+if [ "" ]; then
+echo "SHOULD NOT GET HERE"
+exit
+qsub -pe alloc 3 -l virtual_free=13G -N PIC ~/Work/SGE/qCMD \
+	picard_1119 CollectRnaSeqMetrics R=$GENOME_FASTQ \
+	RIBOSOMAL_INTERVALS=$RIBOFILE \
+	REF_FLAT=$GENESFILE \
+	STRAND=NONE \
+	I=$BAM \
+	O=${BASE}_RNAMetrics.txt
+fi
+
+qsub -pe alloc 3 -l virtual_free=13G -N PIC ~/Work/SGE/qCMD \
+	picard_1119 CollectRnaSeqMetrics R=$GENOME_FASTQ \
+	RIBOSOMAL_INTERVALS=$RIBOFILE \
+	REF_FLAT=$GENESFILE \
+	STRAND=SECOND_READ_TRANSCRIPTION_STRAND \
+	I=$BAM \
+	O=${BASE}_RNAMetrics_SRTS.txt
