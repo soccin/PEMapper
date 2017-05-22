0eaad37ff1744e719e9474f9e2f4c8ed22f3dfa0
Nicholas D. Socci <soccin@mskcc.org>
2017-05-22 18:29:08 -0400

    Propogate options from runPEMult to pipe
    Need to propogate any options from runPEMultiDirectories to pipe; put these at __END__ of command line

--- runPEMapperMultiDirectories.sh
+++ runPEMapperMultiDirectories.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 SDIR="$( cd "$( dirname "$0" )" && pwd )"
+
+if [ "$#" -lt 2 ]; then
+    echo "usage runPEMapperMultiDirectories.sh GENOME MAPPING_FILE [-options to pipe.sh]"
+    exit
+fi
+
 GENOME=$1
 MAPPING=$2
 
-if [ "$#" -ne 2 ]; then
-    echo "usage runPEMapperMultiDirectories.sh GENOME MAPPING_FILE"
-    exit
-fi
+shift 2
 
 for sample in $(cat $MAPPING | cut -f2 | sort | uniq); do
     echo $sample;
     cat $MAPPING | awk -v S=$sample '$2==S{print $4}' \
-        | xargs $SDIR/pipe.sh -s $sample $GENOME
+        | xargs $SDIR/pipe.sh $* -s $sample $GENOME
 done
