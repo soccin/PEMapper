e0dd3cd5198e133b72612b17ca2d2d8d8ab2e4de
Nicholas D. Socci <soccin@mskcc.org>
2018-03-03 17:53:38 -0500

    SE Version of runPEMapper

--- runPEMapperMultiDirectoriesSE.sh
+++ runPEMapperMultiDirectoriesSE.sh
@@ -7,7 +7,7 @@ while getopts "t:" opt; do
         t)  TAG=$OPTARG
             ;;
         \?)
-            echo "usage runPEMapperMultiDirectories.sh [-t TAG] GENOME MAPPING_FILE"
+            echo "usage runPEMapperMultiDirectoriesSE.sh [-t TAG] GENOME MAPPING_FILE"
             exit
             ;;
     esac
@@ -16,7 +16,7 @@ done
 shift $((OPTIND - 1))
 
 if [ "$#" -ne 2 ]; then
-    echo "usage runPEMapperMultiDirectories.sh [-t TAG] GENOME MAPPING_FILE"
+    echo "usage runPEMapperMultiDirectoriesSE.sh [-t TAG] GENOME MAPPING_FILE"
     exit
 fi
 
@@ -26,5 +26,5 @@ MAPPING=$2
 for sample in $(cat $MAPPING | cut -f2 | sort | uniq); do
     echo $sample;
     cat $MAPPING | awk -v S=$sample '$2==S{print $4}' \
-        | xargs $SDIR/pipe.sh -t $TAG -s $sample $GENOME
+        | xargs $SDIR/pipeSE.sh -t $TAG -s $sample $GENOME
 done
