f03784890696171b5edb142117ba51a64b484fed
Nicholas D. Socci <soccin@mskcc.org>
2020-07-02 18:19:49 -0400

    Fix header to SRA format

---
+++ fixHeader.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+SDIR="$( cd "$( dirname "$0" )" && pwd )"
+export PATH=$SDIR/bin:$PATH
+
+BAM=$1
+
+samtools view -H $BAM | egrep -v "^@(SQ)" >${BAM}.hdr
+cat ${GENOME_FASTA/.fa/.dict} | egrep -v "^@HD" >>${BAM}.hdr
+
+picardV2 ReplaceSamHeader I=$BAM O=${BAM/.bam/_FixHdr.bam} HEADER=${BAM}.hdr
+
+picardV2 BuildBamIndex I=${BAM/.bam/_FixHdr.bam}
+
+rm $BAM ${BAM/.bam/.bai}
+
