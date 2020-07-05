a261c425c391ca2ccabe10af1612f638c50f93a8
Nicholas D. Socci <soccin@mskcc.org>
2020-07-05 00:03:13 -0400

    Remove set -e from fixHeader

--- fixHeader.sh
+++ fixHeader.sh
@@ -5,6 +5,7 @@ export PATH=$SDIR/bin:$PATH
 BAM=$1
 
 samtools view -H $BAM | egrep -v "^@(SQ)" >${BAM}.hdr
+
 cat ${GENOME_FASTA/.fa/.dict} | egrep -v "^@HD" >>${BAM}.hdr
 
 picardV2 ReplaceSamHeader I=$BAM O=${BAM/.bam/_FixHdr.bam} HEADER=${BAM}.hdr
