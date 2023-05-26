90d8390d28f3111efe9b5279d7f84beccaa7d0ee
Nicholas D. Socci <soccin@mskcc.org>
2023-05-25 20:19:17 -0400

    New postProcess script

---
+++ bin/postProcessSAM.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+INPUT=$1
+OUTPUT=$2
+
+samtools view -b -o ${INPUT}.1.bam $INPUT
+samtools collate -@ 2 -o ${INPUT}.2.bam ${INPUT}.1.bam
+samtools fixmate -@ 2 -m ${INPUT}.2.bam ${INPUT}.3.bam
+
+picard.local AddOrReplaceReadGroups MAX_RECORDS_IN_RAM=5000000 CREATE_INDEX=true SO=coordinate \
+    LB=$SAMPLENAME PU=${BASE1%%_R1_*} SM=$SAMPLENAME PL=illumina CN=GCL \
+    I={INPUT}.3.bam O=$OUTPUT
