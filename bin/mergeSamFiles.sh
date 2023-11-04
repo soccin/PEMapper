b3fc3fb462cb553ff296f42e94a2444fc750d347
Nicholas D. Socci <soccin@mskcc.org>
2023-11-04 15:21:46 -0400

    wrapper to use /fscratch

---
+++ bin/mergeSamFiles.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#
+# picard.local MergeSamFiles \
+#     MAX_RECORDS_IN_RAM=5000000 SO=coordinate CREATE_INDEX=true \
+#     O=$OUTDIR/${SAMPLENAME}.bam $INPUTS
+#
+
+function uuid_short {
+    uuidgen | md5sum - | perl -ne 's/^(........)/\1/;print $1'
+}
+
+function on_exit {
+    rm -rf $TDIR
+}
+
+OUTPUT=$1
+shift 1
+
+LOG=${OUTPUT}.log
+
+if [ -e /fscratch ]; then
+    TDIR=/fscratch/socci/PEMapper/$(uuid_short)
+else
+    TDIR=/scratch/socci/PEMapper/$(uuid_short)
+fi
+
+echo TDIR=$TDIR | tee -a $LOG
+mkdir -vp $TDIR | tee -a $LOG
+
+picard.local MergeSamFiles \
+     MAX_RECORDS_IN_RAM=5000000 SO=coordinate CREATE_INDEX=true \
+     O=$TDIR/mergeSamFiles.bam $@ | tee -a $LOG
+
+md5sum $TDIR/mergeSamFiles.bam | tee -a $LOG
+rsync -a $TDIR/mergeSamFiles.bam $OUTPUT ${OUTPUT/.bam/.bai} | tee -a $LOG
+sleep 60
+rsync -avP $TDIR/mergeSamFiles.bam $OUTPUT ${OUTPUT/.bam.bai} | tee -a $LOG
+md5sum $OUTPUT | tee -a $LOG
+
+
