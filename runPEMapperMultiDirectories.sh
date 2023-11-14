6a3c4f1bac47e68932a1afb18d4e1c409d8ce6f6
Nicholas D. Socci <soccin@mskcc.org>
2023-11-14 15:17:52 -0500

    Add rate limiter

--- runPEMapperMultiDirectories.sh
+++ runPEMapperMultiDirectories.sh
@@ -24,7 +24,26 @@ GENOME=$1
 MAPPING=$2
 
 for sample in $(cat $MAPPING | cut -f2 | sort | uniq); do
+
+    NUMJOBS=$(bjobs | wc -l);
+    while [ $NUMJOBS -gt 2000 ]; do
+        date
+        echo NUMJOBS=$NUMJOBS;
+        NUMJOBS=$(bjobs | wc -l);
+        sleep 60;
+    done
+
+    echo "========================================"
     echo $sample;
     cat $MAPPING | awk -v S=$sample '$2==S{print $4}' \
         | xargs $SDIR/pipe.sh -t $TAG -s $sample $GENOME
+    echo "done with $sample"
+    echo "========================================"
+
 done
+
+echo
+echo
+echo DONE
+echo
+echo
