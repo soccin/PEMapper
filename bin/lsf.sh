a3726e19d5b485f7b4bb873b3d7ae45a31d00369
Nicholas D. Socci <soccin@mskcc.org>
2023-11-14 15:23:09 -0500

    Added new LSF version; change long time

--- bin/lsf.sh
+++ bin/lsf.sh
@@ -39,7 +39,14 @@ QRUN () {
         10.1)
             TIME_FLAG="-W"
             TIME_SHORT="$TIME_FLAG 59"
-            TIME_LONG="$TIME_FLAG 359"
+            TIME_LONG="$TIME_FLAG 48:00"
+
+        ;;
+
+        34)
+            TIME_FLAG="-W"
+            TIME_SHORT="$TIME_FLAG 59"
+            TIME_LONG="$TIME_FLAG 48:00"
 
         ;;
 
