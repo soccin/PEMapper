d5eb30930e889a1279611005f3fb10ab0f2ab98c
Nicholas D. Socci <soccin@mskcc.org>
2024-04-17 10:50:45 -0400

    Fix bsub.sh script

--- bin/bsub.sh
+++ bin/bsub.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+#
+# This stuff is now handled with the
+# BSUBRC stuff in the ~/bin/bsub
+# script
+#
 #echo bsub -R "select[hname!=lt06]" $*
-bsub -R "select[hname!=lt06]" $*
+
+bsub $*
