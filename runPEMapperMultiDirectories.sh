66ff36d764d3b9d5b95da44eff07790e915a8b9f
Nicholas D. Socci <soccin@mskcc.org>
2024-09-18 14:56:23 -0400

    Added job number limit

--- runPEMapperMultiDirectories.sh
+++ runPEMapperMultiDirectories.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -euo pipefail
+
 SDIR="$( cd "$( dirname "$0" )" && pwd )"
 
 TAG=qPEMAP
@@ -23,8 +26,33 @@ fi
 GENOME=$1
 MAPPING=$2
 
+# for sample in $(cat $MAPPING | cut -f2 | sort | uniq); do
+#     echo $sample;
+#     cat $MAPPING | awk -v S=$sample '$2==S{print $4}' \
+#         | xargs $SDIR/pipe.sh -t $TAG -s $sample $GENOME
+# done
+
 for sample in $(cat $MAPPING | cut -f2 | sort | uniq); do
+
+    NUMJOBS=$(bjobs | wc -l);
+    while [ $NUMJOBS -gt 2000 ]; do
+        date
+        echo NUMJOBS=$NUMJOBS;
+        NUMJOBS=$(bjobs | wc -l);
+        sleep 60;
+    done
+
+    echo "========================================"
     echo $sample;
     cat $MAPPING | awk -v S=$sample '$2==S{print $4}' \
         | xargs $SDIR/pipe.sh -t $TAG -s $sample $GENOME
+    echo "done with $sample"
+    echo "========================================"
+
 done
+
+echo
+echo
+echo DONE
+echo
+echo
