e18b240d6e074d9117b06d248aa01fd640826f1e
Nicholas D. Socci <soccin@mskcc.org>
2024-09-18 05:29:23 -0400

    Checkin postProcess changes

--- bin/postProcessSAM.sh
+++ bin/postProcessSAM.sh
@@ -5,10 +5,22 @@ OUTPUT=$2
 SAMPLENAME=$3
 PU=$4
 
-samtools view -b -o ${INPUT}.1.bam $INPUT
-samtools collate -@ 2 -o ${INPUT}.2.bam ${INPUT}.1.bam
-samtools fixmate -@ 2 -m ${INPUT}.2.bam ${INPUT}.3.bam
+TDIR=/fscratch/socci/PEMapper/$(uuidgen)
+mkdir -vp $TDIR
+if [ ! -e $TDIR ]; then
+    echo no fscratch, using scratch;
+    TDIR=/scratch/socci/PEMapper/$(uuidgen)
+    mkdir -vp $TDIR
+fi
+
+TBAM=$TDIR/$(basename $INPUT)
+
+samtools view -b -o ${TBAM}.1.bam $INPUT
+samtools collate -@ 2 -o ${TBAM}.2.bam ${TBAM}.1.bam
+samtools fixmate -@ 2 -m ${TBAM}.2.bam ${TBAM}.3.bam
 
 picard.local AddOrReplaceReadGroups MAX_RECORDS_IN_RAM=5000000 CREATE_INDEX=true SO=coordinate \
     LB=$SAMPLENAME PU=$PU SM=$SAMPLENAME PL=illumina CN=GCL \
-    I=${INPUT}.3.bam O=$OUTPUT
+    I=${TBAM}.3.bam O=$OUTPUT
+
+rm -vrf $TDIR
